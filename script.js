11d683edfc396f5fcbbeafb83e52a6247649b77e
GABI <gabriellimrqs1998@gmail.com>
2023-05-15 15:48:52 -0300

    interação

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const name = document.querySelector("#nome")
+const heigth = document.querySelector("#altura")
+const weight = document.querySelector("#peso")
+
+
+function calculateIMC(){
+    const imc = Number.heigth.value * weight.value
+    console.log(imc)
+}
