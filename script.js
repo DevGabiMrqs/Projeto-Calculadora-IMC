457312832c4f309c15e030cb1017d4f0d93f7091
Gabrielli <gabriellimrqs1998@gmail.com>
2023-05-16 01:21:06 -0300

    atualização

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
-const name = document.querySelector("#nome")
-const heigth = document.querySelector("#altura")
-const weight = document.querySelector("#peso")
+const name = document.querySelector("#nome");
+const heigth = document.querySelector("#altura");
+const weight = document.querySelector("#peso");
+const results = document.querySelector(".result");
 
 
 function calculateIMC(){
-    const imc = Number.heigth.value * weight.value
-    console.log(imc)
+    const names = name.value;
+    const imc = Number(heigth.value) * weight.value
+    const message =`${names} seu índice de massa corporal é ${imc}.`;
+    results.innerHTML = message;
 }
+
